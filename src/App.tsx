9c11620ed28e2db1c165816860bdc27d9da07580
Jayden <yawzyn001@mymail.sim.edu.sg>
2022-10-14 02:26:21 +0800

    overflow

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,8 @@ import Transaction from "./components/Transaction";
 
 export default function App() {
   return (
-    <div className="w-screen h-screen flex items-center p-4 bg-gray-200 overflow-hidden">
-      <div className="sm:w-[30%] w-full h-full p-4 m-auto overflow-auto">
+    <div className="w-screen h-screen flex items-center p-4 bg-gray-200 overflow-scroll">
+      <div className="sm:w-[30%] w-full h-full p-4 m-auto">
         <h1 className="total font-extrabold text-2xl">Expense Tracker</h1>
         <Balance />
         <Transaction />
